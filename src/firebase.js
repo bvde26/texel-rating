dd2107e547654863909770329cc2d62b7dee702e
Bram van den Elst <bramvdelst@gmail.com>
2026-04-21 13:43:10 +0200

    Use memoryLocalCache to surface Firestore errors immediately

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { initializeFirestore, memoryLocalCache } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { getMessaging, isSupported } from 'firebase/messaging'
 
@@ -13,7 +13,7 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-export const db = getFirestore(app)
+export const db = initializeFirestore(app, { localCache: memoryLocalCache() })
 export const auth = getAuth(app)
 export const getMessagingInstance = async () => {
   const supported = await isSupported()
